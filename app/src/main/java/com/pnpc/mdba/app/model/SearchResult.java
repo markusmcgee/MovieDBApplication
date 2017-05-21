be3028870607ed1ac7e8f492f6ab1587ca5c62ca
Markus <developer@plugandplaycoding.com>
2017-05-20 20:33:46 -0500

    Update - Additional Search items added.

---
+++ app/src/main/java/com/pnpc/mdba/app/model/SearchResult.java
@@ -0,0 +1,45 @@
+
+package com.pnpc.mdba.app.model;
+
+import java.util.List;
+
+public class SearchResult {
+
+    private Integer page;
+    private List<Result> results = null;
+    private Integer totalResults;
+    private Integer totalPages;
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public List<Result> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Result> results) {
+        this.results = results;
+    }
+
+    public Integer getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(Integer totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public Integer getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(Integer totalPages) {
+        this.totalPages = totalPages;
+    }
+
+}
--- app/src/main/java/com/pnpc/mdba/app/service/MovieDBClient.java
+++ app/src/main/java/com/pnpc/mdba/app/service/MovieDBClient.java
@@ -1,6 +1,8 @@
 package com.pnpc.mdba.app.service;
 
+import com.pnpc.mdba.app.model.Genre;
 import com.pnpc.mdba.app.model.Movie;
+import com.pnpc.mdba.app.model.SearchResult;
 
 import io.reactivex.Observable;
 import retrofit2.http.GET;
@@ -16,7 +18,14 @@ public interface MovieDBClient {
     @GET("/3/movie/{id}")
     Observable<Movie> getMovie(@Path("id") int movieId, @Query("api_key") String apiKey);
 
-//    @GET("/movie/550")
-//    Observable<Movie> getMovies();
+    @GET("/genre/movie/list")
+    Observable<Genre> getMovieGenres();
+
+    @GET("/discover/movie")
+    Observable<Genre> movieDiscoverSearch(@Query("api_key") String apiKey, @Query("language") String language, @Query("with_genres") int genre, @Query("include_adult") boolean includeAdult, @Query("include_video") boolean includeVideo);
+
+    @GET()
+    Observable<SearchResult> searchMovies(@Query("api_key") String apiKey, @Query("language") String language, @Query("include_adult") boolean includeAdult, @Query("query") String search_query);
+
 
 }
