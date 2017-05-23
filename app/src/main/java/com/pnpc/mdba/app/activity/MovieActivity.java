3bc93424a6ab87b96e1a0db49244dc1ab01ee883
Markus <developer@plugandplaycoding.com>
2017-05-23 00:57:29 -0500

    Update

--- app/src/main/java/com/pnpc/mdba/app/activity/MovieActivity.java
+++ app/src/main/java/com/pnpc/mdba/app/activity/MovieActivity.java
@@ -3,6 +3,7 @@ package com.pnpc.mdba.app.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.EditText;
